868c6548012c2e61ebb119a37e626fc72ab42416
Sebastian Bohmann <mail@sebastian-bohmann.com>
2021-01-31 10:23:43 +0100

    make tiny fibonacci variant a one-liner

--- javascript/tinyFibonacci.js
+++ javascript/tinyFibonacci.js
@@ -1,3 +1,1 @@
-export default n => {
-    return (y => y(y))(y => (f => x => (x < 3 ? 1 : f(x - 1) + f(x - 2)))(x => y(y)(x)))(n)
-}
+export default n => (y => y(y))(y => (f => x => (x < 3 ? 1 : f(x - 1) + f(x - 2)))(x => y(y)(x)))(n)
