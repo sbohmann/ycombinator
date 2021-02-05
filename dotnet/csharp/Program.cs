810b781e307f556d89317073b7746d1e5947b88b
Sebastian Bohmann <mail@sebastian-bohmann.com>
2021-02-05 11:44:11 +0100

    C#

--- dotnet/csharp/Program.cs
+++ dotnet/csharp/Program.cs
@@ -3,7 +3,7 @@
 namespace csharp {
     static class Program {
         public static void Main(string[] args) {
-            for (int n = 0; n < 10; ++n) {
+            for (var n = 0; n < 10; ++n) {
                 Console.WriteLine(n + " " + Fibonacci.ForIndex(n));
             }
         }
