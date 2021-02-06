a04035632efc9c8553e7e2738f5a04d3c51a58c9
sbohmann <mail@sebastian-bohmann.com>
2021-02-06 22:12:57 +0100

    clojure formatting

--- clojure/main.clj
+++ clojure/main.clj
@@ -2,11 +2,11 @@
 (load-file "./generic.clj")
 
 (defn makeFibonacci [f]
-              (fn [x]
-                (cond
-                 (= x 0) 0
-                 (= x 1) 1
-                 :else   (+ (f (- x 1)) (f (- x 2))))))
+  (fn [x]
+    (cond
+     (= x 0) 0
+     (= x 1) 1
+     :else   (+ (f (- x 1)) (f (- x 2))))))
 
 (doseq [n (range 0 10)]
   (.println System/out (str n \space (fibonacci n) \space ((generic makeFibonacci) n))))
