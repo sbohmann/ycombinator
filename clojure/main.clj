50e67086831f3c31846173403b86dbdfb26ed46f
sbohmann <mail@sebastian-bohmann.com>
2021-02-06 21:46:44 +0100

    clojure formatting

--- clojure/main.clj
+++ clojure/main.clj
@@ -4,9 +4,9 @@
                   (cond
                    (= x 0) 0
                    (= x 1) 1
-                   :else (+ (f (- x 1)) (f(- x 2))))))]
+                   :else   (+ (f (- x 1)) (f (- x 2))))))]
     (let [y (fn [y] (fn [x] ((makeF (y y)) x)))]
-          ((y y) n))))
+      ((y y) n))))
 
 (doseq [n (range 0 10)]
   (.println System/out (str n \space (fibonacci n))))
