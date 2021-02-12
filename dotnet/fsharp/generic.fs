f1e364100487dec7dcfa0e8568fe14eb98346e9b
sbohmann <mail@sebastian-bohmann.com>
2021-02-12 17:03:47 +0100

    F# now works

--- dotnet/fsharp/generic.fs
+++ dotnet/fsharp/generic.fs
@@ -3,5 +3,5 @@ module generic
 type Y = { call: Y -> (int -> int) }
 
 let combine makeF =
-    let y = { call = fun y -> fun x -> makeF(y.call(y)) x }
-    (y.call y)
+    let y = { call = fun y -> fun x -> (makeF (y.call y)) x }
+    y.call y
